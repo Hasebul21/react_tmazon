e94a92f9716ad24850644973e2335fc70e16aa9a
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-26 10:11:55 +0600

    prettier setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,37 @@
-import { createStore } from "redux";
+import { createStore } from 'redux'
 
 const initialState = {
   count: 0,
   showCounter: true,
-};
+}
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
-      console.log("Incrementing")
+    case 'INCREMENT':
+      console.log('Incrementing')
       return {
         ...state,
         count: state.count + 1,
-      };
-    case "DECREMENT":
+      }
+    case 'DECREMENT':
       return {
         ...state,
         count: state.count - 1,
-      };
-    case "INCREMENT_BY_5":
+      }
+    case 'INCREMENT_BY_5':
       return {
         ...state,
         count: state.count + action.amount,
-      };
-    case "TOGGLE_COUNTER":
+      }
+    case 'TOGGLE_COUNTER':
       return {
         ...state,
         showCounter: !state.showCounter,
-      };
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-const store = createStore(counterReducer);
-export default store;
+const store = createStore(counterReducer)
+export default store
