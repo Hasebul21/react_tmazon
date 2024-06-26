8bffa4746f0eb97304dc4f9d6b0b121f9d9c99cf
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-26 10:26:43 +0600

    prettier setup

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,9 +7,7 @@ export default function Counter() {
   const incrementBy10 = () => {dispatch({ type: 'INCREMENT_BY_5', amount: 10 })}
   const togglrButton = () => {dispatch({ type: 'TOGGLE_COUNTER' })}
   return (
-    <div>
-      <button onClick={incrementCounter}>Increment</button>
-      <button onClick={decrementCounter}>Decrement</button>
+    <div><button onClick={incrementCounter}>Increment</button><button onClick={decrementCounter}>Decrement</button>
       <button onClick={incrementBy10}>Increment By 10</button>
       <button onClick={togglrButton}>Toogle</button>
     </div>
