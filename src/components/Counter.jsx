a0fb70b5ce4b02b5bf770a08c395a7b3ad9d32b3
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-26 11:11:53 +0600

    feature/30/any messgae

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,8 @@ export default function Counter() {
   const dispatch = useDispatch()
   const incrementCounter = () => {dispatch({ type: 'INCREMENT' })}
   const decrementCounter = () => {dispatch({ type: 'DECREMENT' })}
-  const incrementBy10 = () => {dispatch({ type: 'INCREMENT_BY_5', amount: 10 })}
+  const incrementBy10 = () => {dispatch({ type: 'INCREMENT_BY_5', amount: 10 })
+}
   const togglrButton = () => {
     dispatch({ type: 'TOGGLE_COUNTER' })
   }
