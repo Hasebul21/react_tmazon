cc1bab3995676ba02680dd32fe11acb2cb40f8a5
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 16:23:47 +0600

    ANP-1234 Fxing Bug for me what you are you doing i am now where

--- e2e/check.js
+++ e2e/check.js
@@ -1,3 +1,6 @@
 const x = 10
 console.log('This is a test')
-console.log('This is a test');console.log('This is a test')
+console.log('This is a test')
+console.log('This is a test')
+console.log('This is a test')
+console.log('This is a test')
