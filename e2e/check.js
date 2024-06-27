a6071d4b0f9d5bbe7e7cdd830efa173634d20c73
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 15:30:35 +0600

    AN-1234 Fxing Bug for me what you are you doing

--- e2e/check.js
+++ e2e/check.js
@@ -1,4 +1,1 @@
-const x = 10
-console.log('This is a test')
-console.log('ldldldld')
-console.log('') // Add an empty console.log statement to complete the expression
+const x = 10;console.log('This is a test');console.log('ldldldld')
--- src/context/CounterContext.jsx
+++ src/context/CounterContext.jsx
@@ -2,27 +2,18 @@ import { createContext } from 'react'
 import { useState } from 'react'
 
 export const CounterContext = createContext({
-  counter: 0,
-  incrementCounter: () => {},
-  decrementCounter: () => {},
+  counter: 0,incrementCounter: () => {},decrementCounter: () => {},
 })
 
 export default function CounterContextProvider({ children }) {
   const [counter, setCounter] = useState(0)
 
-  function increment() {
-    setCounter((counter) => counter + 1)
-  }
+  function increment() {setCounter((counter) => counter + 1)}
 
-  function decrement() {
-    setCounter((counter) => counter - 1)
-  }
+  function decrement() {setCounter((counter) => counter - 1)}
 
   const ctx = {
-    counter,
-    incrementCounter: increment,
-    decrementCounter: decrement,
-  }
+    counter,incrementCounter: increment,decrementCounter: decrement,}
 
   return (
     <CounterContext.Provider value={ctx}>{children}</CounterContext.Provider>
