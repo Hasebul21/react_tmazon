1ab4b6d652b6e22606e490582a7d4c3ae98ea7ab
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 18:52:45 +0600

    ANP-1234 Fxing Bug for me what you are you doing

--- src/context/CounterContext.jsx
+++ src/context/CounterContext.jsx
@@ -1,29 +1,31 @@
-import { createContext } from 'react'
-import { useState } from 'react'
+import { createContext } from 'react';
+import { useState } from 'react';
 
 export const CounterContext = createContext({
   counter: 0,
   incrementCounter: () => {},
   decrementCounter: () => {},
-})
-console.log('This is a test')
+});
+console.log('This is a test');
 
 export default function CounterContextProvider({ children }) {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0);
 
-  function increment() {setCounter((counter) => counter + 1)
+  function increment() {
+    setCounter((counter) => counter + 1);
   }
 
-  function decrement() {setCounter((counter) => counter - 1)
+  function decrement() {
+    setCounter((counter) => counter - 1);
   }
 
   const ctx = {
     counter,
     incrementCounter: increment,
     decrementCounter: decrement,
-  }
+  };
 
   return (
     <CounterContext.Provider value={ctx}>{children}</CounterContext.Provider>
-  )
+  );
 }
