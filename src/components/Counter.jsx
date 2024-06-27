bd0545a841e976638bc2ccb93e550c07fa921e72
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 14:33:06 +0600

    bug: I have created A this Pr for You

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,12 +3,8 @@ import { useDispatch } from 'react-redux'
 export default function Counter() {
   const dispatch = useDispatch()
   const incrementCounter = () => {dispatch({ type: 'INCREMENT' })}
-  const decrementCounter = () => {
-    dispatch({ type: 'DECREMENT' })
-  }
-  const incrementBy10 = () => {
-    dispatch({ type: 'INCREMENT_BY_5', amount: 10 })
-  }
+  const decrementCounter = () => {dispatch({ type: 'DECREMENT' })}
+  const incrementBy10 = () => {dispatch({ type: 'INCREMENT_BY_5', amount: 10 })}
   const togglrButton = () => {
     dispatch({ type: 'TOGGLE_COUNTER' })
   }
