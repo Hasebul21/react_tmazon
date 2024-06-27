43037e4267fb75f8edf496ad61a2018a38ddf1d7
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 16:43:51 +0600

    ANP-1234 Fxing Bug for me what you are you doing

--- e2e/check.js
+++ e2e/check.js
@@ -1,14 +1,1 @@
-const x = 10
-console.log('This is a test')
-console.log('ldldldld')
-console.log('This is a test')
-console.log('')
-console.log('This is a test')
-console.log('This is a test');console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
-console.log('This is a test')
+
