7261d83e5d32400baf123ab3b178c58a23acd764
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-27 14:08:19 +0600

    AN-1234 Fxing Bug

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,8 +2,12 @@ import { useDispatch } from 'react-redux'
 
 export default function Counter() {
   const dispatch = useDispatch()
-  const incrementCounter = () => {dispatch({ type: 'INCREMENT' })}
-  const decrementCounter = () => {dispatch({ type: 'DECREMENT' })}
+  const incrementCounter = () => {
+    dispatch({ type: 'INCREMENT' })
+  }
+  const decrementCounter = () => {
+    dispatch({ type: 'DECREMENT' })
+  }
   const incrementBy10 = () => {
     dispatch({ type: 'INCREMENT_BY_5', amount: 10 })
   }
