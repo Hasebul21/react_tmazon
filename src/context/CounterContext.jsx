18841103f4df7e95c94a973380856a5448b1ae11
Hasebul21 <hasebulhassan21@gmail.com>
2024-06-28 19:07:31 +0600

    hotfix: Hello this is my commit

--- src/context/CounterContext.jsx
+++ src/context/CounterContext.jsx
@@ -11,12 +11,10 @@ console.log('This is a test');
 export default function CounterContextProvider({ children }) {
   const [counter, setCounter] = useState(0);
 
-  function increment() {
-    setCounter(counter => counter + 1);
+  function increment() {setCounter(counter => counter + 1);
   }
 
-  function decrement() {
-    setCounter(counter => counter - 1);
+  function decrement() {setCounter(counter => counter - 1);
   }
   const ctx = {
     counter,
